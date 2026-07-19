5de951ae17ea0197f197527119a10a433da1ff1b
mkc1687 <mkc1687@gmail.com>
2026-07-19 11:01:47 +0800

    v2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import operator as op
 import tkinter as tk
 from tkinter import ttk
 
-# test
+# test3
 class SafeEvaluator(ast.NodeVisitor):
     """Evaluate a small arithmetic expression safely."""
 
